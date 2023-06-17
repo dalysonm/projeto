535028f87417d7a81f8cbe482a40772f3c9fd3c7
Dalyuson Moreira <dalysonlucas@gmail.com>
2023-06-17 15:07:32 -0300

    add readme

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function toggleMode() {
   // pegar a tag img
   const img = document.querySelector("#profile img")
 
-  // substituir a  imagem
+  // substituir a imagem
   if (html.classList.contains("light")) {
-    //se tiver light mode, adiconar a imagem light
+    // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    //se tiver sem light mode, manter a imagem norma
+    // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
   }
 }
